c4fad7aeab419cdfb83e6d02601451e0bd126fe6
anandajuliar <anandajulia001@gmail.com>
2026-03-09 19:49:42 +0700

    praktikum 1 dan 2

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -9,8 +9,8 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100 text-[16px] leading-loose">
-                    Nama : Nachma Olivia <br>
-                    NIM : 20230140048 <br>
+                    Nama : Ananda Julia <br>
+                    NIM : 20230140046 <br>
                     Program Studi : Teknologi Informasi <br>
                 </div>
             </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tugas Pertemuan 1 & 2</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-[#0f0f0f] min-h-screen flex items-center justify-center p-6 relative">
 
@@ -23,8 +24,8 @@
     @endif
 
     <div class="w-full max-w-4xl border border-neutral-800 bg-[#171717] rounded-lg p-10 shadow-lg">
-        <h1 class="text-white font-bold text-lg mb-1">Nachma Olivia</h1>
-        <p class="text-neutral-400 text-sm mb-4">20230140048</p>
+        <h1 class="text-white font-bold text-lg mb-1">Ananda Julia</h1>
+        <p class="text-neutral-400 text-sm mb-4">20230140046</p>
         <button class="bg-white text-black text-sm font-semibold py-2 px-4 rounded hover:bg-gray-200 transition">
             Modul Pertemuan 1
         </button>
